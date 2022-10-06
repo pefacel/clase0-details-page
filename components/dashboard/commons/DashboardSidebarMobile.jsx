34ac92a09b3922b975272762a22e227de395d6ad
Pefacel <pefacel@gmail.com>
2022-10-06 20:02:20 -0300

    f

--- components/dashboard/commons/DashboardSidebarMobile.jsx
+++ components/dashboard/commons/DashboardSidebarMobile.jsx
@@ -1,128 +1,151 @@
-import { Fragment, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import {
- 
-    XMarkIcon,
-  } from '@heroicons/react/24/outline'
-import { classNames } from '../../../lib/Helper'
- export default function DashboardSidebarMobile({sidebarOpen, setSidebarOpen,navigation,secondaryNavigation,user}) {
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { classNames } from "../../../lib/Helper";
+export default function DashboardSidebarMobile({
+  sidebarOpen,
+  setSidebarOpen,
+  navigation,
+  secondaryNavigation,
+  user,
+}) {
   return (
     <Transition.Root show={sidebarOpen} as={Fragment}>
-    <Dialog as="div" className="relative z-40 lg:hidden" onClose={setSidebarOpen}>
-      <Transition.Child
-        as={Fragment}
-        enter="transition-opacity ease-linear duration-300"
-        enterFrom="opacity-0"
-        enterTo="opacity-100"
-        leave="transition-opacity ease-linear duration-300"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
+      <Dialog
+        as="div"
+        className="relative z-40 lg:hidden"
+        onClose={setSidebarOpen}
       >
-        <div className="fixed inset-0 bg-gray-600 bg-opacity-75" />
-      </Transition.Child>
-
-      <div className="fixed inset-0 z-40 flex">
         <Transition.Child
           as={Fragment}
-          enter="transition ease-in-out duration-300 transform"
-          enterFrom="-translate-x-full"
-          enterTo="translate-x-0"
-          leave="transition ease-in-out duration-300 transform"
-          leaveFrom="translate-x-0"
-          leaveTo="-translate-x-full"
+          enter="transition-opacity ease-linear duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity ease-linear duration-300"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-white focus:outline-none">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-in-out duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="ease-in-out duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <div className="absolute top-0 right-0 -mr-12 pt-2">
-                <button
-                  type="button"
-                  className="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                  onClick={() => setSidebarOpen(false)}
-                >
-                  <span className="sr-only">Close sidebar</span>
-                  <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
-                </button>
-              </div>
-            </Transition.Child>
-            <div className="h-0 flex-1 overflow-y-auto pt-5 pb-4">
-              <div className="flex flex-shrink-0 items-center px-4">
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=pink&shade=500"
-                  alt="Your Company"
-                />
-              </div>
-              <nav aria-label="Sidebar" className="mt-5">
-                <div className="space-y-1 px-2">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className={classNames(
-                        item.current
-                          ? 'bg-gray-100 text-gray-900'
-                          : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
-                        'group flex items-center px-2 py-2 text-base font-medium rounded-md'
-                      )}
-                      aria-current={item.current ? 'page' : undefined}
-                    >
-                      <item.icon
-                        className={classNames(
-                          item.current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500',
-                          'mr-4 h-6 w-6'
-                        )}
-                        aria-hidden="true"
-                      />
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-                <hr className="my-5 border-t border-gray-200" aria-hidden="true" />
-                <div className="space-y-1 px-2">
-                  {secondaryNavigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="group flex items-center rounded-md px-2 py-2 text-base font-medium text-gray-600 hover:bg-gray-50 hover:text-gray-900"
-                    >
-                      <item.icon
-                        className="mr-4 h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                        aria-hidden="true"
-                      />
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-              </nav>
-            </div>
-            <div className="flex flex-shrink-0 border-t border-gray-200 p-4">
-              <a href="#" className="group block flex-shrink-0">
-                <div className="flex items-center">
-                  <div>
-                    <img className="inline-block h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
-                  </div>
-                  <div className="ml-3">
-                    <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">{user.name}</p>
-                    <p className="text-sm font-medium text-gray-500 group-hover:text-gray-700">View profile</p>
-                  </div>
-                </div>
-              </a>
-            </div>
-          </Dialog.Panel>
+          <div className="fixed inset-0 bg-gray-600 bg-opacity-75" />
         </Transition.Child>
-        <div className="w-14 flex-shrink-0" aria-hidden="true">
-          {/* Force sidebar to shrink to fit close icon */}
+
+        <div className="fixed inset-0 z-40 flex">
+          <Transition.Child
+            as={Fragment}
+            enter="transition ease-in-out duration-300 transform"
+            enterFrom="-translate-x-full"
+            enterTo="translate-x-0"
+            leave="transition ease-in-out duration-300 transform"
+            leaveFrom="translate-x-0"
+            leaveTo="-translate-x-full"
+          >
+            <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-white focus:outline-none">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-in-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in-out duration-300"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="absolute top-0 right-0 -mr-12 pt-2">
+                  <button
+                    type="button"
+                    className="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                    onClick={() => setSidebarOpen(false)}
+                  >
+                    <span className="sr-only">Close sidebar</span>
+                    <XMarkIcon
+                      className="h-6 w-6 text-white"
+                      aria-hidden="true"
+                    />
+                  </button>
+                </div>
+              </Transition.Child>
+              <div className="h-0 flex-1 overflow-y-auto pt-5 pb-4">
+                <div className="flex flex-shrink-0 items-center px-4">
+                  <img
+                    className="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=pink&shade=500"
+                    alt="Your Company"
+                  />
+                </div>
+                <nav aria-label="Sidebar" className="mt-5">
+                  <div className="space-y-1 px-2">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-600 hover:bg-gray-50 hover:text-gray-900",
+                          "group flex items-center px-2 py-2 text-base font-medium rounded-md"
+                        )}
+                        aria-current={item.current ? "page" : undefined}
+                      >
+                        <item.icon
+                          className={classNames(
+                            item.current
+                              ? "text-gray-500"
+                              : "text-gray-400 group-hover:text-gray-500",
+                            "mr-4 h-6 w-6"
+                          )}
+                          aria-hidden="true"
+                        />
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                  <hr
+                    className="my-5 border-t border-gray-200"
+                    aria-hidden="true"
+                  />
+                  <div className="space-y-1 px-2">
+                    {secondaryNavigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="group flex items-center rounded-md px-2 py-2 text-base font-medium text-gray-600 hover:bg-gray-50 hover:text-gray-900"
+                      >
+                        <item.icon
+                          className="mr-4 h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                          aria-hidden="true"
+                        />
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </nav>
+              </div>
+              <div className="flex flex-shrink-0 border-t border-gray-200 p-4">
+                <a href="#" className="group block flex-shrink-0">
+                  <div className="flex items-center">
+                    <div>
+                      <img
+                        className="inline-block h-10 w-10 rounded-full"
+                        src={user.imageUrl}
+                        alt=""
+                      />
+                    </div>
+                    <div className="ml-3">
+                      <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">
+                        {user.name}
+                      </p>
+                      <p className="text-sm font-medium text-gray-500 group-hover:text-gray-700">
+                        View profile
+                      </p>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </Dialog.Panel>
+          </Transition.Child>
+          <div className="w-14 flex-shrink-0" aria-hidden="true">
+            {/* Force sidebar to shrink to fit close icon */}
+          </div>
         </div>
-      </div>
-    </Dialog>
-  </Transition.Root>
-  )
+      </Dialog>
+    </Transition.Root>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,160 +1,141 @@
- 
-import {   useState } from 'react'
+import { useState } from "react";
 import {
   Bars3Icon,
-  CalendarIcon,
   CogIcon,
   HomeIcon,
-  MagnifyingGlassCircleIcon,
-  MapIcon,
-  MegaphoneIcon,
   SquaresPlusIcon,
-  UserGroupIcon,
- } from '@heroicons/react/24/outline'
-import { ChevronLeftIcon  } from '@heroicons/react/20/solid'
-import DashboardSidebarDesktop from '../components/dashboard/commons/DashboardSidebarDesktop'
- import DashboardSidebarMobile from '../components/dashboard/commons/DashboardSidebarMobile'
-import DirectoryList from '../components/dashboard/directory/DirectoryList'
-import DirectoryMember from '../components/dashboard/directory/DirectoryMember'
+} from "@heroicons/react/24/outline";
+import DashboardSidebarDesktop from "../components/dashboard/commons/DashboardSidebarDesktop";
+import DashboardSidebarMobile from "../components/dashboard/commons/DashboardSidebarMobile";
+import DirectoryList from "../components/dashboard/directory/DirectoryList";
+import DirectoryMember from "../components/dashboard/directory/DirectoryMember";
+import DashboardTitleMobile from "../components/dashboard/commons/DashboardTitleMobile";
 
 const user = {
-  name: 'Tom Cook',
+  name: "Tom Cook",
   imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
+    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+};
 const navigation = [
-  { name: 'Dashboard', href: '/', icon: HomeIcon, current: false },
-  { name: 'Form', href: '/form', icon: HomeIcon, current: false },
-
- 
-]
+  { name: "Dashboard", href: "/", icon: HomeIcon, current: false },
+  { name: "Form", href: "/form", icon: HomeIcon, current: false },
+];
 const secondaryNavigation = [
-  { name: 'Apps', href: '#', icon: SquaresPlusIcon },
-  { name: 'Settings', href: '#', icon: CogIcon },
-]
+  { name: "Apps", href: "#", icon: SquaresPlusIcon },
+  { name: "Settings", href: "#", icon: CogIcon },
+];
 const tabs = [
-  { name: 'Profile', href: '#', current: true },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Recognition', href: '#', current: false },
-]
-const profile = {
-  name: 'Ricardo Cooper',
-  imageUrl:
-    'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-  coverImageUrl:
-    'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
-  about: `
-    <p>Tincidunt quam neque in cursus viverra orci, dapibus nec tristique. Nullam ut sit dolor consectetur urna, dui cras nec sed. Cursus risus congue arcu aenean posuere aliquam.</p>
-    <p>Et vivamus lorem pulvinar nascetur non. Pulvinar a sed platea rhoncus ac mauris amet. Urna, sem pretium sit pretium urna, senectus vitae. Scelerisque fermentum, cursus felis dui suspendisse velit pharetra. Augue et duis cursus maecenas eget quam lectus. Accumsan vitae nascetur pharetra rhoncus praesent dictum risus suspendisse.</p>
-  `,
-  fields: {
-    Phone: '(555) 123-4567',
-    Email: 'ricardocooper@example.com',
-    Title: 'Senior Front-End Developer',
-    Team: 'Product Development',
-    Location: 'San Francisco',
-    Sits: 'Oasis, 4th floor',
-    Salary: '$145,000',
-    Birthday: 'June 8, 1990',
-  },
-}
-const directory = 
-[{
-  name: 'Ricardo Cooper',
-  imageUrl:
-    'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-  coverImageUrl:
-    'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
-  about: `
-    <p>Tincidunt quam neque in cursus viverra orci, dapibus nec tristique. Nullam ut sit dolor consectetur urna, dui cras nec sed. Cursus risus congue arcu aenean posuere aliquam.</p>
-    <p>Et vivamus lorem pulvinar nascetur non. Pulvinar a sed platea rhoncus ac mauris amet. Urna, sem pretium sit pretium urna, senectus vitae. Scelerisque fermentum, cursus felis dui suspendisse velit pharetra. Augue et duis cursus maecenas eget quam lectus. Accumsan vitae nascetur pharetra rhoncus praesent dictum risus suspendisse.</p>
-  `,
-  fields: {
-    Phone: '(555) 123-4567',
-    Email: 'ricardocooper@example.com',
-    Title: 'Senior Front-End Developer',
-    Team: 'Product Development',
-    Location: 'San Francisco',
-    Sits: 'Oasis, 4th floor',
-    Salary: '$145,000',
-    Birthday: 'June 8, 1990',
-  },
-}]
+  { name: "Profile", href: "#", current: true },
+  { name: "Calendar", href: "#", current: false },
+  { name: "Recognition", href: "#", current: false },
+];
 
+const profile = {
+  name: "Ricardo Cooper",
+  imageUrl:
+    "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+  coverImageUrl:
+    "https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+  about: `
+    <p>Tincidunt quam neque in cursus viverra orci, dapibus nec tristique. Nullam ut sit dolor consectetur urna, dui cras nec sed. Cursus risus congue arcu aenean posuere aliquam.</p>
+    <p>Et vivamus lorem pulvinar nascetur non. Pulvinar a sed platea rhoncus ac mauris amet. Urna, sem pretium sit pretium urna, senectus vitae. Scelerisque fermentum, cursus felis dui suspendisse velit pharetra. Augue et duis cursus maecenas eget quam lectus. Accumsan vitae nascetur pharetra rhoncus praesent dictum risus suspendisse.</p>
+  `,
+  fields: {
+    Phone: "(555) 123-4567",
+    Email: "ricardocooper@example.com",
+    Title: "Senior Front-End Developer",
+    Team: "Product Development",
+    Location: "San Francisco",
+    Sits: "Oasis, 4th floor",
+    Salary: "$145,000",
+    Birthday: "June 8, 1990",
+  },
+};
+
+const directory = [
+  {
+    name: "Ricardo Cooper",
+    imageUrl:
+      "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+    coverImageUrl:
+      "https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+    about: `
+    <p>Tincidunt quam neque in cursus viverra orci, dapibus nec tristique. Nullam ut sit dolor consectetur urna, dui cras nec sed. Cursus risus congue arcu aenean posuere aliquam.</p>
+    <p>Et vivamus lorem pulvinar nascetur non. Pulvinar a sed platea rhoncus ac mauris amet. Urna, sem pretium sit pretium urna, senectus vitae. Scelerisque fermentum, cursus felis dui suspendisse velit pharetra. Augue et duis cursus maecenas eget quam lectus. Accumsan vitae nascetur pharetra rhoncus praesent dictum risus suspendisse.</p>
+  `,
+    fields: {
+      Phone: "(555) 123-4567",
+      Email: "ricardocooper@example.com",
+      Title: "Senior Front-End Developer",
+      Team: "Product Development",
+      Location: "San Francisco",
+      Sits: "Oasis, 4th floor",
+      Salary: "$145,000",
+      Birthday: "June 8, 1990",
+    },
+  },
+];
 
 const team = [
   {
-    name: 'Leslie Alexander',
-    handle: 'lesliealexander',
-    role: 'Co-Founder / CEO',
+    name: "Leslie Alexander",
+    handle: "lesliealexander",
+    role: "Co-Founder / CEO",
     imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
-    name: 'Michael Foster',
-    handle: 'michaelfoster',
-    role: 'Co-Founder / CTO',
+    name: "Michael Foster",
+    handle: "michaelfoster",
+    role: "Co-Founder / CTO",
     imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
-    name: 'Dries Vincent',
-    handle: 'driesvincent',
-    role: 'Manager, Business Relations',
+    name: "Dries Vincent",
+    handle: "driesvincent",
+    role: "Manager, Business Relations",
     imageUrl:
-      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
   {
-    name: 'Lindsay Walton',
-    handle: 'lindsaywalton',
-    role: 'Front-end Developer',
+    name: "Lindsay Walton",
+    handle: "lindsaywalton",
+    role: "Front-end Developer",
     imageUrl:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
-]
-
- 
+];
 
 export default function Example() {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <>
- 
       <div className="flex h-full">
-<DashboardSidebarMobile sidebarOpen={sidebarOpen}  setSidebarOpen={setSidebarOpen} navigation={navigation} secondaryNavigation ={secondaryNavigation} user={user} />
+        <DashboardSidebarMobile
+          sidebarOpen={sidebarOpen}
+          setSidebarOpen={setSidebarOpen}
+          navigation={navigation}
+          secondaryNavigation={secondaryNavigation}
+          user={user}
+        />
 
-<DashboardSidebarDesktop navigation={navigation} secondaryNavigation={secondaryNavigation} user={user} />
+        <DashboardSidebarDesktop
+          navigation={navigation}
+          secondaryNavigation={secondaryNavigation}
+          user={user}
+        />
 
         <div className="flex min-w-0 flex-1 flex-col overflow-hidden">
-          <div className="lg:hidden">
-            <div className="flex items-center justify-between border-b border-gray-200 bg-gray-50 px-4 py-1.5">
-              <div>
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=pink&shade=500"
-                  alt="Your Company"
-                />
-              </div>
-              <div>
-                <button
-                  type="button"
-                  className="-mr-3 inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-pink-600"
-                  onClick={() => setSidebarOpen(true)}
-                >
-                  <span className="sr-only">Open sidebar</span>
-                  <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-                </button>
-              </div>
-            </div>
-          </div>
+          <DashboardTitleMobile setSidebarOpen={setSidebarOpen} />
           <div className="relative z-0 flex flex-1 overflow-hidden">
-          <DirectoryMember profile={profile} tabs={tabs} team={team} />
+            <DirectoryMember profile={profile} tabs={tabs} team={team} />
 
- <DirectoryList directory={directory} />
+            <DirectoryList directory={directory} />
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
